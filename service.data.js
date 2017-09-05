e78ae9528c1025fbf8f581b03c8ea14f1b5e48a6
prospecs <prospecs@umich.edu>
2017-09-05 03:52:25 -0400

    processEnemies and reformat

--- service.data.js
+++ service.data.js
@@ -5,22 +5,40 @@ app.service('DataService', ['$rootScope', function($rootScope) {
     const gridWidth = 1;
     var progress = 0;
 
-	var characters = null;
-	var enemies = null;
+    var characters = null;
+    var enemies = null;
     var rows = [];
     var cols = [];
     var map, characterData, enemyData, itemIndex, skillIndex, coordMapping, terrainIndex, terrainLocs;
 
-    this.getCharacters = function() { return characters; };
-	this.getEnemies = function() { return enemies; };
-    this.getMap = function() { return map; };
-    this.getRows = function() { return rows; };
-    this.getColumns = function(){ return cols; };
-    this.getTerrainTypes = function() { return terrainIndex; };
-    this.getTerrainMappings = function() { return terrainLocs; };
+    this.getCharacters = function() {
+        return characters;
+    };
+    this.getEnemies = function() {
+        return enemies;
+    };
+    this.getMap = function() {
+        return map;
+    };
+    this.getRows = function() {
+        return rows;
+    };
+    this.getColumns = function() {
+        return cols;
+    };
+    this.getTerrainTypes = function() {
+        return terrainIndex;
+    };
+    this.getTerrainMappings = function() {
+        return terrainLocs;
+    };
 
-    this.loadMapData = function() { fetchCharacterData(); };
-    this.calculateRanges = function() { getMapDimensions(); };
+    this.loadMapData = function() {
+        fetchCharacterData();
+    };
+    this.calculateRanges = function() {
+        getMapDimensions();
+    };
 
     //\\//\\//\\//\\//\\//
     // DATA AJAX CALLS  //
@@ -46,18 +64,18 @@ app.service('DataService', ['$rootScope', function($rootScope) {
             // Obtain all images from all columns at row 5
             range: 'Stats!B5:AZ5',
         }).then(function(response) {
-			var images = response.result.values[0];
-			
-			for(var i = 0; i < images.length && i < characterData.length; i++){
-				characterData[i].splice(4, 1, processImageURL(images[i])); //replace the element at index 4
-			}
+            var images = response.result.values[0];
 
-			updateProgressBar();
-			fetchEnemyData();
+            for (var i = 0; i < images.length && i < characterData.length; i++) {
+                characterData[i].splice(4, 1, processImageURL(images[i])); //replace the element at index 4
+            }
+
+            updateProgressBar();
+            fetchEnemyData();
         });
-	}
-	
-	function fetchEnemyData() {
+    }
+
+    function fetchEnemyData() {
         gapi.client.sheets.spreadsheets.values.get({
             spreadsheetId: sheetId,
             majorDimension: "COLUMNS",
@@ -77,100 +95,100 @@ app.service('DataService', ['$rootScope', function($rootScope) {
             // Obtain all images from all columns at row 5
             range: 'Enemy Stats!B5:AZ5',
         }).then(function(response) {
-			var images = response.result.values[0];
-			
-			for(var i = 0; i < images.length && i < enemyData.length; i++){
-				enemyData[i].splice(3, 1, processImageURL(images[i])); //replace the element at index 3
-			}
+            var images = response.result.values[0];
 
-			updateProgressBar();
-			fetchItemIndex();
+            for (var i = 0; i < images.length && i < enemyData.length; i++) {
+                enemyData[i].splice(3, 1, processImageURL(images[i])); //replace the element at index 3
+            }
+
+            updateProgressBar();
+            fetchItemIndex();
         });
-	}
-	
-	function fetchItemIndex() {
+    }
+
+    function fetchItemIndex() {
         gapi.client.sheets.spreadsheets.values.get({
             spreadsheetId: sheetId,
             majorDimension: "ROWS",
             range: 'Item List!B2:AO',
         }).then(function(response) {
-			var results = response.result.values;
-			
-			itemIndex = {};
-			for(var i = 0; i < results.length; i++){
-				var itm = results[i];
-				if(itm[0].length > 0){ //if the item has a name
-					itemIndex[itm[0]] = {
-						'name' : itm[0],
-						'type' : itm[1],
-						'atkStat' : itm[2],
-						'rank' : itm[3],
-						'might' : parseInt(itm[4]) | 0,
-						'hit' : parseInt(item[5]) | 0,
-						'crit' : parseInt(itm[6]) | 0,
-						'crit%' : parseFloat(itm[7]) | 0.0,
-						'critDmg' : parseInt(itm[8]) | 0,
-						'avo' : parseInt(itm[9]) | 0,
-						'cEva' : parseInt(itm[10]) | 0,
-						'range' : itm[11],
-						'effect' : itm[12],
-						'effective' : itm[13],
-						'StrEqpt' : parseInt(itm[14]) | 0,
-						'MagEqpt' : parseInt(itm[15]) | 0,
-						'SklEqpt' : parseInt(itm[16]) | 0,
-						'SpdEqpt' : parseInt(itm[17]) | 0,
-						'LckEqpt' : parseInt(itm[18]) | 0,
-						'DefEqpt' : parseInt(itm[19]) | 0,
-						'MagEqpt' : parseInt(itm[20]) | 0,
-						'StrInv' : parseInt(itm[28]) | 0,
-						'MagInv' : parseInt(itm[29]) | 0,
-						'SklInv' : parseInt(itm[30]) | 0,
-						'SpdInv' : parseInt(itm[31]) | 0,
-						'LckInv' : parseInt(itm[32]) | 0,
-						'DefInv' : parseInt(itm[33]) | 0,
-						'MagInv' : parseInt(itm[34]) | 0,
-						'desc' : itm[35] != undefined ? itm[35] : "",
-						'spriteUrl' : itm[36] != undefined ? itm[36] : "",
-					}
-				}
-			}
+            var results = response.result.values;
+
+            itemIndex = {};
+            for (var i = 0; i < results.length; i++) {
+                var itm = results[i];
+                if (itm[0].length > 0) { //if the item has a name
+                    itemIndex[itm[0]] = {
+                        'name': itm[0],
+                        'type': itm[1],
+                        'atkStat': itm[2],
+                        'rank': itm[3],
+                        'might': parseInt(itm[4]) | 0,
+                        'hit': parseInt(item[5]) | 0,
+                        'crit': parseInt(itm[6]) | 0,
+                        'crit%': parseFloat(itm[7]) | 0.0,
+                        'critDmg': parseInt(itm[8]) | 0,
+                        'avo': parseInt(itm[9]) | 0,
+                        'cEva': parseInt(itm[10]) | 0,
+                        'range': itm[11],
+                        'effect': itm[12],
+                        'effective': itm[13],
+                        'StrEqpt': parseInt(itm[14]) | 0,
+                        'MagEqpt': parseInt(itm[15]) | 0,
+                        'SklEqpt': parseInt(itm[16]) | 0,
+                        'SpdEqpt': parseInt(itm[17]) | 0,
+                        'LckEqpt': parseInt(itm[18]) | 0,
+                        'DefEqpt': parseInt(itm[19]) | 0,
+                        'MagEqpt': parseInt(itm[20]) | 0,
+                        'StrInv': parseInt(itm[28]) | 0,
+                        'MagInv': parseInt(itm[29]) | 0,
+                        'SklInv': parseInt(itm[30]) | 0,
+                        'SpdInv': parseInt(itm[31]) | 0,
+                        'LckInv': parseInt(itm[32]) | 0,
+                        'DefInv': parseInt(itm[33]) | 0,
+                        'MagInv': parseInt(itm[34]) | 0,
+                        'desc': itm[35] != undefined ? itm[35] : "",
+                        'spriteUrl': itm[36] != undefined ? itm[36] : "",
+                    }
+                }
+            }
 
             updateProgressBar();
             fetchSkillIndex();
         });
-	};
-	
-	function fetchSkillIndex() {
+    };
+
+    function fetchSkillIndex() {
         gapi.client.sheets.spreadsheets.values.get({
             spreadsheetId: sheetId,
             majorDimension: "ROWS",
             range: 'Skill List!B2:C',
         }).then(function(response) {
-			var skills = response.result.values;
-			
-			gapi.client.sheets.spreadsheets.values.get({
-				spreadsheetId: sheetId,
-				majorDimension : "COLUMNS",
-				valueRenderOption: "FORMULA",
-				range: 'Skill List!A2:A',
-			}).then(function(response){
-				var images = response.results.values[0];
+            var skills = response.result.values;
 
-				skillIndex = {};
-				for(var i = 0; i < skills.length; i++){
-					var s = skills[i];
-					if(s[0].length > 0){ //if the item has a name
-						skillIndex[s[0]] = {
-							'name' : s[0],
-							'desc' : s[1],
-							'spriteUrl' : images[i] != undefined ? processImageURL(images[i]) : ""
-						}
-					}
-				}
+            gapi.client.sheets.spreadsheets.values.get({
+                spreadsheetId: sheetId,
+                majorDimension: "COLUMNS",
+                valueRenderOption: "FORMULA",
+                range: 'Skill List!A2:A',
+            }).then(function(response) {
+                var images = response.results.values[0];
 
-				updateProgressBar();
-				processCharacters();
-			});
+                skillIndex = {};
+                for (var i = 0; i < skills.length; i++) {
+                    var s = skills[i];
+                    if (s[0].length > 0) { //if the item has a name
+                        skillIndex[s[0]] = {
+                            'name': s[0],
+                            'desc': s[1],
+                            'spriteUrl': images[i] != undefined ? processImageURL(images[i]) : ""
+                        }
+                    }
+                }
+
+                updateProgressBar();
+                processCharacters();
+            });
         });
     };
 
@@ -221,18 +239,18 @@ app.service('DataService', ['$rootScope', function($rootScope) {
 
         for (var i = 0; i < characterData.length; i++) {
             var c = characterData[i];
-			if(c[0].length == 0) continue;
+            if (c[0].length == 0) continue;
 
             var currObj = {
                 'name': c[0],
-				'class': c[1],
-				'unitType' : c[2],
+                'class': c[1],
+                'unitType': c[2],
                 'spriteUrl': c[3],
                 'level': c[4],
-				'exp': c[5],
-				'gold' : parseInt(c[6].substring(0, c[6].index("|")).trim()) | 0,
-				'ore' : parseInt(c[6].substring(c[6].index("|")+1).trim()) | 0,
-				'position': c[7],
+                'exp': c[5],
+                'gold': parseInt(c[6].substring(0, c[6].index("|")).trim()) | 0,
+                'ore': parseInt(c[6].substring(c[6].index("|") + 1).trim()) | 0,
+                'position': c[7],
                 'currHp': parseInt(c[9]) | 0,
                 'maxHp': parseInt(c[10]) | 0,
                 'StrPair': parseInt(c[11]) | 0,
@@ -242,26 +260,26 @@ app.service('DataService', ['$rootScope', function($rootScope) {
                 'LckPair': parseInt(c[15]) | 0,
                 'DefPair': parseInt(c[16]) | 0,
                 'ResPair': parseInt(c[17]) | 0,
-				'MovPair': parseInt(c[18]) | 0,
-				'StrBase' : parseInt(c[20]) | 0,
-				'MagBase' : parseInt(c[21]) | 0,
-				'SklBase' : parseInt(c[22]) | 0,
-				'SpdBase' : parseInt(c[23]) | 0,
-				'LckBase' : parseInt(c[24]) | 0,
-				'DefBase' : parseInt(c[25]) | 0,
-				'MagBase' : parseInt(c[26]) | 0,
-				'MovBase' : parseInt(c[27]) | 0,
-				'weaknesses' : c[28].length > 0 && c[28] != "-" ? c[28].split(",") : [],
-				'atk' : parseInt(c[29]) | 0,
-				'hit' : parseInt(c[30]) | 0,
-				'crit' : parseInt(c[31]) | 0,
-				'avo' : parseInt(c[32]) | 0,
-				'cEva' : parseInt(c[33]) | 0,
-				'inventory' : {},
-				'partner' : c[42],
-				'stance' : c[43],
-				'shields': c[44],
-				'skills' : {},
+                'MovPair': parseInt(c[18]) | 0,
+                'StrBase': parseInt(c[20]) | 0,
+                'MagBase': parseInt(c[21]) | 0,
+                'SklBase': parseInt(c[22]) | 0,
+                'SpdBase': parseInt(c[23]) | 0,
+                'LckBase': parseInt(c[24]) | 0,
+                'DefBase': parseInt(c[25]) | 0,
+                'MagBase': parseInt(c[26]) | 0,
+                'MovBase': parseInt(c[27]) | 0,
+                'weaknesses': c[28].length > 0 && c[28] != "-" ? c[28].split(",") : [],
+                'atk': parseInt(c[29]) | 0,
+                'hit': parseInt(c[30]) | 0,
+                'crit': parseInt(c[31]) | 0,
+                'avo': parseInt(c[32]) | 0,
+                'cEva': parseInt(c[33]) | 0,
+                'inventory': {},
+                'partner': c[42],
+                'stance': c[43],
+                'shields': c[44],
+                'skills': {},
                 'hpBuff': parseInt(c[54]) | 0,
                 'StrBuff': parseInt(c[55]) | 0,
                 'MagBuff': parseInt(c[56]) | 0,
@@ -270,12 +288,12 @@ app.service('DataService', ['$rootScope', function($rootScope) {
                 'LckBuff': parseInt(c[59]) | 0,
                 'DefBuff': parseInt(c[60]) | 0,
                 'ResBuff': parseInt(c[61]) | 0,
-				'MovBuff': parseInt(c[62]) | 0,
-				'atkBuff' : parseInt(c[63]) | 0,
-				'hitBuff' : parseInt(c[64]) | 0,
-				'critBuff' : parseInt(c[65]) | 0,
-				'avoBuff' : parseInt(c[66]) | 0,
-				'cEvaBuff' : parseInt(c[67]) | 0,
+                'MovBuff': parseInt(c[62]) | 0,
+                'atkBuff': parseInt(c[63]) | 0,
+                'hitBuff': parseInt(c[64]) | 0,
+                'critBuff': parseInt(c[65]) | 0,
+                'avoBuff': parseInt(c[66]) | 0,
+                'cEvaBuff': parseInt(c[67]) | 0,
                 'weaponRanks': {
                     'w1': {
                         'class': c[83],
@@ -293,6 +311,94 @@ app.service('DataService', ['$rootScope', function($rootScope) {
                         'exp': calcExpPercent(c[91])
                     }
                 },
+            };
+
+            //Find and append weapons
+
+            currObj.skills.skl0 = findSkill(skls[skls.length - 1], personalSkillsDesc);
+
+            for (var j = 0; j < skls.length - 1; j++)
+                currObj.skills["skl" + (j + 1)] = findSkill(skls[j], skillDescriptions);
+
+            charaObjs["char_" + i] = currObj;
+        }
+
+        updateProgressBar();
+        fetchMapUrl();
+    };
+
+    // WIP
+    function processEnemies() {
+        enemies = {};
+
+        for (var i = 0; i < enemyData.length; i++) {
+            var e = enemyData[i];
+            if (e[0].length == 0) continue;
+
+            var currObj = {
+                'name': e[0],
+                'class': e[1],
+                'unitType': e[2],
+                'spriteUrl': e[3],
+                'currHp': parseInt(e[5]) | 0,
+                'maxHp': parseInt(e[6]) | 0,
+                'StrPair': parseInt(e[7]) | 0,
+                'MagPair': parseInt(e[8]) | 0,
+                'SklPair': parseInt(e[9]) | 0,
+                'SpdPair': parseInt(e[10]) | 0,
+                'LckPair': parseInt(e[11]) | 0,
+                'DefPair': parseInt(e[12]) | 0,
+                'ResPair': parseInt(e[13]) | 0,
+                'MovPair': parseInt(e[14]) | 0,
+                'StrBase': parseInt(e[16]) | 0,
+                'MagBase': parseInt(e[17]) | 0,
+                'SklBase': parseInt(e[18]) | 0,
+                'SpdBase': parseInt(e[19]) | 0,
+                'LckBase': parseInt(e[20]) | 0,
+                'DefBase': parseInt(e[21]) | 0,
+                'MagBase': parseInt(e[22]) | 0,
+                'MovBase': parseInt(e[23]) | 0,
+                'level': e[24],
+                'position': e[25],
+                'atk': parseInt(e[26]) | 0,
+                'hit': parseInt(e[27]) | 0,
+                'crit': parseInt(e[28]) | 0,
+                'avo': parseInt(e[29]) | 0,
+                'cEva': parseInt(e[30]) | 0,
+                'weaknesses': e[35].length > 0 && e[35] != "-" ? e[35].split(",") : [],
+                'inventory': {},
+                'weaponRanks': {
+                    'w1': {
+                        'class': e[40],
+                        'rank': e[42],
+                    },
+                    'w2': {
+                        'class': e[86],
+                        'rank': e[87],
+                    },
+                    'w3': {
+                        'class': c[89],
+                        'rank': c[90],
+                    }
+                },
+                'skills': {},
+                'partner': e[55],
+                'stance': e[56],
+                'shields': e[57],
+                'hpBuff': parseInt(e[59]) | 0,
+                'StrBuff': parseInt(e[60]) | 0,
+                'MagBuff': parseInt(e[61]) | 0,
+                'SklBuff': parseInt(e[62]) | 0,
+                'SpdBuff': parseInt(e[63]) | 0,
+                'LckBuff': parseInt(e[64]) | 0,
+                'DefBuff': parseInt(e[65]) | 0,
+                'ResBuff': parseInt(e[66]) | 0,
+                'MovBuff': parseInt(e[67]) | 0,
+                'atkBuff': parseInt(e[68]) | 0,
+                'hitBuff': parseInt(e[69]) | 0,
+                'critBuff': parseInt(e[70]) | 0,
+                'avoBuff': parseInt(e[71]) | 0,
+                'cEvaBuff': parseInt(e[72]) | 0,
             };
 
             //Find and append weapons
@@ -509,20 +615,20 @@ app.service('DataService', ['$rootScope', function($rootScope) {
     };
 
     function processImageURL(str) {
-		if(str == undefined || str.length == 0) return "";
+        if (str == undefined || str.length == 0) return "";
         else return str.substring(str.indexOf("\"") + 1, str.lastIndexOf("\""));
-	};
-	
-	function calcExpPercent(exp){
-		if(exp.length < 3) return 0;
+    };
 
-		var split = exp.split("/");
-		var curr = parseInt(split[0].trim()) | 0;
-		var nextRank = parseInt(split[1].trim()) | 0;
+    function calcExpPercent(exp) {
+        if (exp.length < 3) return 0;
 
-		if(curr == 0 || nextRank == 0) return 0;
-		else return curr/nextRank;
-	};
+        var split = exp.split("/");
+        var curr = parseInt(split[0].trim()) | 0;
+        var nextRank = parseInt(split[1].trim()) | 0;
+
+        if (curr == 0 || nextRank == 0) return 0;
+        else return curr / nextRank;
+    };
 
     function getItem(name) {
         var originalName = name;
